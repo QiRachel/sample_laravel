a6b59832de59b016b2f8ccc7fa141fe5352cfbe6
holiday <577128666@qq.com>
2017-11-07 13:55:18 +0000

    finish layout and routes

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -3,9 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title', 'Sample')</title>
-
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layout._header')
+    <div class="container">
+      @yield('content')
+    </div>
+    @include('layout._footer')
   </body>
 </html>
